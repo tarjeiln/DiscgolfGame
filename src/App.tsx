d2bf3882bdc014aec937c5455cc77f2133dc89eb
tarjeiln <tarjeiln@stud.ntnu.no>
2025-09-11 14:52:21 +0200

    build: :building_construction: Project creation
    Created project with React TS and css file

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Discgolf (lokal MVP)</h1>
+      <button>Kast!</button>
+    </div>
+  )
+}
+
+export default App
